26789b0e562c5223c28fc23606589d1001066ea0
Robert Zablit <rzablit@gmail.com>
2023-05-03 14:45:34 -0700

    Add and configure Firebase SDK
    The dependency was added by running `npx expo install firebase`. A basic
    initialization was copied from Expo's documentation:
    https://docs.expo.dev/guides/using-firebase/#initialize-the-sdk-in-your-project.
    
    Note that we're hard-coding values here since they are an old dev
    environment that we'll be deleting soon. Longer-term we'll have these be
    defined dynamically by environment variables.

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
 import 'expo-dev-client';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { initializeApp } from 'firebase/app';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDCcjiqeswjCIqqk5LJkDJfGwTJNQDd9go",
+  authDomain: "hodor-development-60a50.firebaseapp.com",
+  projectId: "hodor-development-60a50",
+  storageBucket: "hodor-development-60a50.appspot.com",
+  messagingSenderId: "624053831444",
+  appId: "1:624053831444:web:505d6a555d8528ebe16401"
+}
+
+initializeApp(firebaseConfig);
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
